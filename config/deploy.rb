3442fef957e207ecc161bf0d95c5ce4c216dba34
Aaron Hill <armahillo@gmail.com>
2016-06-16 21:37:23 -0400

    Makes .htaccess a linked file

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :deploy_to, "/home1/#{fetch(:user)}/rails_apps/#{fetch(:application)}"
 set :format, :pretty
 
 set :tmp_dir, "/home1/pdxdiape/rails_apps/capistrano_tmp"
-set :linked_files, fetch(:linked_files, []).push('.env')
+set :linked_files, fetch(:linked_files, []).push('.env', 'public/.htaccess')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'vendor/bundle', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
