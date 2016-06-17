6b417aa5cfe4793450752e39f6d89b12fee0d335
Aaron H <armahillo@gmail.com>
2016-06-17 16:35:56 -0400

    Update ticket_spec.rb

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,14 +1,5 @@
 require "rails_helper"
 
 RSpec.describe Ticket, type: :model do
-  it "has a date" do
-    ticket = Ticket.create
-    expect(ticket.created_at).to_not be nil
-  end
-
-  it "has an id" do
-    ticket = Ticket.create(id: 5)
-    expect(ticket.id).to_not be nil
-  end
-
+  
 end
