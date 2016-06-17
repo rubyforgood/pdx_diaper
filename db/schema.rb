41ad5263fad696d391b1ba5f24b7d7cbe438ae6e
katiesteiner <katiewsteiner@gmail.com>
2016-06-17 16:28:02 -0400

    Ticket model with no associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617200323) do
+ActiveRecord::Schema.define(version: 20160617202619) do
 
   create_table "donations", force: true do |t|
     t.string   "dropoff_location"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20160617200323) do
     t.string   "name"
     t.string   "category"
     t.string   "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tickets", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
