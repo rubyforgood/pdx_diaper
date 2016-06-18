c0355f4763ed00e5e78dafb47644444bf988b198
katiesteiner <katiewsteiner@gmail.com>
2016-06-18 19:33:54 -0400

    Tickets now properly throw errors if you select items not available at storage location

--- app/admin/inventory.rb
+++ app/admin/inventory.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Inventory do
   permit_params :name, :address
 
-  show do
+  show do |inventory|
   	@items = Inventory.includes(:holdings).where(id: resource.id).first.items
     
     attributes_table do
@@ -10,6 +10,11 @@ ActiveAdmin.register Inventory do
     end
 
     panel "Items at this location" do
+      ul do
+        inventory.holdings.each do |holding|
+          li "#{holding.item.name} - #{holding.quantity}"
+        end
+      end
     end
 
   end
