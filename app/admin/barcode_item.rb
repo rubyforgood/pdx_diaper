18679f9b8929246a38a9dec93f407bfbb3e63dc3
Christine Movius <cmovius@ord.blueraster.com>
2016-06-18 16:36:43 -0400

    Start custom barcode form.

--- app/admin/barcode_item.rb
+++ app/admin/barcode_item.rb
@@ -3,15 +3,14 @@ ActiveAdmin.register BarcodeItem do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :value, :item_id
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+permit_params :value, :item_id, :quantity
+
+form do |f|
+  inputs 'Add New Item' do
+    input :item_id, :label => 'Item Type', :as => :select, :collection => Item.all
+    input :quantity, :label => 'Quantity', :as => 
+  end
+end
 
 
 end
