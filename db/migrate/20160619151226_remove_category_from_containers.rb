0d9d85b8853ef65e4b67f0946fa2de33ef91a3e7
Keith Walters <keith.walters@cattywamp.us>
2016-06-19 12:07:53 -0400

    Removes duplicate migration
    There are two migrations that remove `:category` from the `containers`
    table. This removes the newer one. Devs should probably rebuild their
    local databases.

--- db/migrate/20160619151226_remove_category_from_containers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoryFromContainers < ActiveRecord::Migration
-  def change
-    remove_column :containers, :category, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160619151226) do
+ActiveRecord::Schema.define(version: 20160619145554) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
