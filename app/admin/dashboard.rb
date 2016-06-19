879f35998d44d65ff5b7689a4ab6187789594f4e
Thomas Baird <tbaird@mail.umw.edu>
2016-06-19 05:49:52 +0000

    Adding datepicker fields

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,12 +2,20 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    section "Filter", :priority => 4 do
-      div do
-
+  sidebar :options do
+      form_for :date, { :url => "#" } do |f|
+        div do
+          f.label :starts_at
+          f.input :starts_at, :as => :datepicker, :class => "datepicker hasDatetimePicker", :value  => (Date.today - 1.year).strftime('%Y-%m-%d')
+        end
+        div do
+          f.label :ends_at
+          f.input :ends_at, :as => :datepicker, :class => "datepicker hasDatetimePicker", :value  => Date.today.strftime('%Y-%m-%d')
+        end
       end
     end
+
+  content title: proc{ I18n.t("active_admin.dashboard") } do
     columns do
       column do
         panel "Inventory Summary" do
@@ -19,19 +27,21 @@ ActiveAdmin.register_page "Dashboard" do
      column do
         panel "Quick Summary" do
           para do
-            h4 "Donation Totals"
-            ul "Totals By Source:" do
+            h3 "Donation Totals"
+            h5 "Totals By Source:" 
+            ul do
               li "Diaper Drive: #{diaper_totals_by_source('Diaper Drive')}"
               li "Purchased Supplies: #{diaper_totals_by_source('Purchased Supplies')}"
               li "Donation Pickup Location: #{diaper_totals_by_source('Donation Pickup Location')}"
             end
-            ul "Totals By Location:" do
+            h5 "Totals By Location:" 
+            ul do
               totals = dropoff_totals
               totals.each do |key, value|
                 li "#{key}: #{value}"
               end
             end
-            h4 "Ticket Totals"
+            h3 "Ticket Totals"
 
           end
         end
