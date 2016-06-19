c601091dee3a95ae9bec7e6068a9760cea4e320a
Christine Movius <cmovius@ord.blueraster.com>
2016-06-19 09:49:04 -0400

    Add barcodes to Item show view.

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -15,21 +15,40 @@ ActiveAdmin.register Item do
 
   menu parent: "Inventory", label: "Item types"
 
-  permit_params :name, :category, :size
+  permit_params :name, :category, :size, :barcode_items_attributes => [:id, :item_id, :quantity, :value]
 
   filter :inventories, label: "Stored In"
   filter :name
   filter :category, as: :select
   filter :created_at
 
-  index do 
+  index do
     selectable_column
     column :name
     column :category
     column "Barcode Entries" do |i|
       entries = BarcodeItem.where(item_id: i).count
-      link_to_unless(entries == 0, entries, barcode_items_path(utf8:"✓", "q[item_id_eq]": i, item_id_eq: i, commit: "Filter", order: "id_desc")) 
+      link_to_unless(entries == 0, entries, barcode_items_path(utf8:"✓", "q[item_id_eq]": i, item_id_eq: i, commit: "Filter", order: "id_desc"))
     end
     actions
   end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :category
+      row :created_at
+      row :updated_at
+      row "BARCODES" do |item|
+        if item.barcode_items.count > 0
+          table_for resource.barcode_items do
+            column :value
+            column :quantity
+          end
+        end
+      end
+    end
+  end
+
 end
